01c28cc806ea9c1f863cb05c430346469109a3cd
naddeai <165570733+naddeai@users.noreply.github.com>
2024-04-21 12:22:52 +0200

    Create naddeai.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/naddeai.py
@@ -0,0 +1,10 @@
+# la URL de Python es https://es.python.org/
+# se puede comentar con # delante del comentario o entre """ """"
+var_1 = "python"
+a_string = "cadena de caracteres"
+b_int = " numeros enteros"
+c_float = 'decimales'
+d_bool = ' verdadero,falso'
+
+print(Hola, Python!)
+
